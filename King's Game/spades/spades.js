7ef06060e8c418b464f2d243c87a48fc5bb88e10
Skaf <101594355+KSkaf@users.noreply.github.com>
2022-09-29 14:37:11 +0300

    Add files via upload
    Pc Version

---
+++ King's Game/spades/spades.js
@@ -0,0 +1,18 @@
+function check(){
+  const nextBtn = document.querySelector('.next');
+  ans=' jack';
+  var input = document.getElementById("myInput").value;
+          if(input == ans){
+              alert("Correct Jack is the killer...he left the kitchen in shock in order to look less suspicious");
+              alert("Joker left in hurry in order to call the police to arrest Jack");
+              alert("Two head out to cut a rose to offer to the Queen"); 
+              window.location = href= "../doors.html"
+          } 
+          else{
+              alert("Sorry. That is the incorrect answer");
+          }
+  }
+
+
+
+
